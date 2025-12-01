d2fb283e3594c8abbd6f2516a8cf0b2048707a49
Jeremiah Tesfaye <jeremiahtesfaye3@gmail.com>
2025-12-01 15:06:58 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,31 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from deep_translator import GoogleTranslator
+
+# Replace this with your actual bot token
+BOT_TOKEN = '8337582417:AAGilEnO6-aenJVtNGIfOUVeBALOcB8g35U'
+
+# /start command handler
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hi! Send me a message in any language and I'll translate it to English.")
+
+async def translate_message(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    original_text = update.message.text
+
+    try:
+        translated_text = GoogleTranslator(source='auto', target='en').translate(original_text)
+        await update.message.reply_text(f'''Translated:\n {translated_text}''')
+    except Exception as e:
+        await update.message.reply_text("Sorry, I coudn't translate")
+
+
+def main():
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler('start',start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, translate_message))
+    print('bot is running...')
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
